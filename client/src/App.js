a7c52b99d84e59374764d6f66bd7a9b926a97d24
bhaktibuana <bhaktibuana19@gmail.com>
2021-10-19 16:37:49 +0700

    3rd commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
 import Form from './Form';
+import { BrowserRouter as Switch, Route } from 'react-router-dom'
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
-  return <Form />;
+  return (
+    <Switch>
+      <Route path='/'>
+        <p>asdasd</p>
+        {/* <Form /> */}
+      </Route>
+      <Route path='/signup'>
+        <SignUpPage />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
